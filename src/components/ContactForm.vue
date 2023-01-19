7bacbc17a00d10aa21630f5828d8027388c63a3a
klelia <clelia.fradella@gmail.com>
2023-01-19 12:17:18 +0100

    validazioni ed errori

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,23 +2,36 @@
     <section class="contact_me py-5">
         <div class="inner-wrapper">
             <div class="container-fluid text-center">
-                <h2 class="text-uppercase">contact me</h2>                
+                <h2 class="text-uppercase">contact me</h2> 
+                <div v-if="success" class="alert alert-success text-start" role="alert">
+                    Messaggio inviato con successo!
+                </div>               
                 <div class="row">
-                    <form class="col-12 text-start">
+                    <form class="col-12 text-start" @submit.prevent="sendForm()">
                         <div class="mb-3">
                             <input class="border-0 border-bottom form-control" 
-                                type="text" name="name" id="name" placeholder="Name">
+                                type="text" name="name" id="name" placeholder="Name" v-model="name" :class="{'is-invalid': errors.name}" required>
+                                <p v-for="(error,index) in errors.name" :key="index" class="invalid-feedback">
+                                    {{ error }}
+                                </p>
                         </div>
                         <div class="mb-3">
                             <input class="border-0 border-bottom form-control" 
-                                type="text" name="email" id="email" placeholder="Email">
+                                type="text" name="email" id="email" placeholder="Email" v-model="email" :class="{'is-invalid': errors.email}" required>
+                                <p v-for="(error,index) in errors.email" :key="index" class="invalid-feedback">
+                                    {{ error }}
+                                </p>
+
                         </div>
                         <div class="mb-3">
                             <textarea class="border-0 border-bottom form-control"
                                name="message" id="message" cols="30"
-                                rows="10" placeholder="Message"></textarea>
+                                rows="10" placeholder="Message" v-model="message" :class="{'is-invalid': errors.message}" required></textarea>
+                                <p v-for="(error,index) in errors.message" :key="index" class="invalid-feedback">
+                                    {{ error }}
+                                </p>
                         </div>
-                        <button class="btn btn-lg btn-primary text-white" type="submit" >Send</button>
+                        <button class="btn btn-lg btn-primary text-white" type="submit" :disabled="loading">{{loading ? 'Sending...' : 'Send'}}</button>
                     </form>
                 </div>
             </div>
@@ -38,11 +51,13 @@ import {store} from '../store';
                 email: '',
                 message: '',
                 success: false,
-                errors: {}
+                errors: {},
+                loading: false               
             }
         },
         methods: {
             sendForm(){
+                this.loading = true;
                 const data = {
                     name: this.name,
                     email: this.email,
@@ -50,6 +65,16 @@ import {store} from '../store';
                 }
                 axios.post(`${this.store.apiBaseUrl}/contacts`, data).then((response)=>{
                     console.log(response.data);
+                    this.success = response.data.success;
+                    if(!this.success){
+                        this.errors = response.data.errors;
+                        console.log(this.errors);
+                    } else {
+                        this.name = '';
+                        this.email = '';
+                        this.message = '';
+                    }
+                    this.loading = false;
                 });
             }
         }
