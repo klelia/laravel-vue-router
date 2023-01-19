4d96a81dcf7067dc4d5051ddae5c58d87b4f277b
klelia <clelia.fradella@gmail.com>
2023-01-19 11:22:56 +0100

    contact form base

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="contact_me py-5">
+        <div class="inner-wrapper">
+            <div class="container-fluid text-center">
+                <h2 class="text-uppercase">contact me</h2>                
+                <div class="row">
+                    <form class="col-12 text-start">
+                        <div class="mb-3">
+                            <input class="border-0 border-bottom form-control" 
+                                type="text" name="name" id="name" placeholder="Name">
+                        </div>
+                        <div class="mb-3">
+                            <input class="border-0 border-bottom form-control" 
+                                type="text" name="email" id="email" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <textarea class="border-0 border-bottom form-control"
+                               name="message" id="message" cols="30"
+                                rows="10" placeholder="Message"></textarea>
+                        </div>
+                        <button class="btn btn-lg btn-primary text-white" type="submit" >Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+import {store} from '../store';
+    export default {
+        name: 'ContactForm',
+        data(){
+            return {
+                store,
+                name: '',
+                email: '',
+                message: '',
+                success: false,
+                errors: {}
+            }
+        },
+        methods: {
+            sendForm(){
+                const data = {
+                    name: this.name,
+                    email: this.email,
+                    message: this.message
+                }
+                axios.post(`${this.store.apiBaseUrl}/contacts`, data).then((response)=>{
+                    console.log(response.data);
+                });
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,12 +1,23 @@
 <template>
-    <div>
-        ContactUs
+    <section>
+    <div class="row">
+     <div class="col-12">
+        <h1 class="mt-5 mb-3">
+            Contatti
+        </h1>
+        <ContactForm />
+     </div>
     </div>
+    </section>
 </template>
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
     export default {
-        name:'ContactUs'
+        name:'ContactUs',
+        components: {
+            ContactForm
+        }
     }
 </script>
 
